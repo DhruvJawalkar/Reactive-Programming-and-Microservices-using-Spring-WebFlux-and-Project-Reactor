29cac160ee93623e5793021f0f5c735886ea815a
Dhruv Jawalkar <dhruv.jawalkar@gmail.com>
2024-09-17 14:44:20 -0700

    Added simple flux, mono and streaming endpoints at /flux, /mono and /stream to explore the data and reactive return types.

---
+++ simple-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
@@ -0,0 +1,33 @@
+package com.reactivespring.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.awt.*;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> flux(){
+        return Flux.just(1,2,3)
+                .log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> mono(){
+        return Mono.just("Hello world!")
+                .log();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream(){
+        return Flux.interval(Duration.of(1, ChronoUnit.SECONDS))
+                .log();
+    }
+}
